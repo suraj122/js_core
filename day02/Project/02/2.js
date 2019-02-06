239c877cefd3a06c024ce14bcca5f3f292d2196c
suraj_122 <122suraj0@gmail.com>
2019-02-06 12:28:20 +0530

    done day02 project

--- day02/Project/02/2.js
+++ day02/Project/02/2.js
@@ -1,32 +1,46 @@
 // What's the output
 
 "hello world" === "hello world";
-// Output
+// Output true
 
 "hello world" == "hello world";
+// true
 
 true === true;
+// true
 
 77 === "77";
+// false
 
 "cat" === "dog";
+// false
 
 false === 0;
+// false
 
 false == 0;
+// true
 
 0 == "";
+// true
 
 "" == false;
+// true
 
 null == null;
+// true;
 
 undefined == undefined;
+// true
 
 null == undefined;
+// true
 
 NaN == null;
+// false
 
 NaN == undefined;
+// false
 
 NaN == NaN;
+// false
