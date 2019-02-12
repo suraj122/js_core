2683f21278133d8ca2db52777c98c56bd9e25561
suraj_122 <122suraj0@gmail.com>
2019-02-12 20:11:50 +0530

    solve problemns on string methods

--- day05/Project/01/script.js
+++ day05/Project/01/script.js
@@ -1,81 +1,72 @@
 // Write a JavaScript function to check whether an `input` is a *string or not*.
 
-// function checkString (str) {
-// 	if(typeof(str) === 'string') {
-// 		console.log("A string");
-// 	} else {
-// 		console.log("Not a string");
-// 	}
-// }
+function checkString (str) {
+	if(typeof(str) === 'string') {
+		console.log("A string");
+	} else {
+		console.log("Not a string");
+	}
+}
 
-// checkString("apple");
+checkString("apple");
 
 // Write a JavaScript function to check whether a string *is blank or not*.
 
-// function checkBlankStr (str) {
-// 	if(Boolean(str) === false) {
-// 		console.log("An empty string");
-// 	} else {
-// 		console.log("Not an empty string");
-// 	}
-// }
-// checkBlankStr("ala");
+function checkBlankStr (str) {
+	if(Boolean(str) === false) {
+		console.log("An empty string");
+	} else {
+		console.log("Not an empty string");
+	}
+}
+checkBlankStr("ala");
 
 // Write a JavaScript *function to split a string and convert it into an array* of words.
 
-// function splitString(str) {
-// 	console.log(str.split(' '));
-// }
-// splitString("ramesh is good boy");
+function splitString(str) {
+	console.log(str.split(' '));
+}
+splitString("ramesh is good boy");
 
 
 // Write a JavaScript function to remove specified number of characters from a string.
 
-// function truncateString(str) {
-// 	console.log(str.slice(2));
-// }
-// truncateString("ramesh");
+function truncateString(str) {
+	console.log(str.slice(2));
+}
+truncateString("ramesh");
 
 
 // Write a JavaScript function *to convert* a string in *abbreviated form*.
-// function abbrevName(str) {
-// 	var splittedStr = str.split(' ');
-// 	var slicedStr = splittedStr[1].slice(0, 1);
-// 	console.log(splittedStr[0] + " " + slicedStr);
-// }
+function abbrevName(str) {
+	var splittedStr = str.split(' ');
+	var slicedStr = splittedStr[1].slice(0, 1);
+	console.log(splittedStr[0] + " " + slicedStr);
+}
 
-// abbrevName("Sasikant kumar");
+abbrevName("Sasikant kumar");
 
 // Write a JavaScript function to hide email addresses to protect from unauthorized user.
 
-// function protectEmail(email) {
-// 	var slicedEmail = email.slice(0, 4);
-// 	var slicedEmail1 = email.slice(email.indexOf("@"));
-// 	var paddedEmail = slicedEmail.padEnd(slicedEmail.length + 3, ".")
-// 	console.log(paddedEmail + slicedEmail1);
-// }
+function protectEmail(email) {
+	var slicedEmail = email.slice(0, 4);
+	var slicedEmail1 = email.slice(email.indexOf("@"));
+	var paddedEmail = slicedEmail.padEnd(slicedEmail.length + 3, ".")
+	console.log(paddedEmail + slicedEmail1);
+}
 
-// protectEmail("sasikant_kvsn@gmail.com");
+protectEmail("sasikant_kvsn@gmail.com");
 
 // Write a JavaScript function to *parameterize a string*.
 
-// function stringParametrize(str) {
-// 	console.log(str.replace(/ /g, "-"));
-// }
+function stringParametrize(str) {
+	console.log(str.replace(/ /g, "-"));
+}
 
 // stringParametrize("ramesh is good boy");
 
 // Write a JavaScript function to *capitalize the first letter of a string*.
 
-function capitalize(str) {
-	var splitStr = str.split(" ");
-	for(let i = 0; i < splitStr.length - 1; i++) {
-		console.log(splitStr[i]);
-	}
-	// console.log(splitStr);
-}
-
-capitalize("ramesh is good boy");
 
 
 
